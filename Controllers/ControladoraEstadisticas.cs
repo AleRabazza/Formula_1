8f4418686f8d923771921f575c554c603b05b31b
JLluviera <juan.labolso@gmail.com>
2024-12-02 17:01:44 -0300

    innecesario

--- Controllers/ControladoraEstadisticas.cs
+++ Controllers/ControladoraEstadisticas.cs
@@ -37,52 +37,6 @@ namespace Formula_1.Controllers
             return View("Estadistica");
         }
 
-        // GET: ControladoraEstadisticas/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: ControladoraEstadisticas/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ControladoraEstadisticas/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ControladoraEstadisticas/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ControladoraEstadisticas/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        
     }
 }
